8c836331ee1546478fc19ed490206bac113e2d48
Sarah Elhwarii <56038677+sarahelhwarii@users.noreply.github.com>
2020-05-11 02:50:57 +0300

    input user's pattern

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+userClickedPattern = [];
 gamePattern = [];
 buttonColors = ["red", "blue", "green", "yellow"];
 function nextSequence() {
@@ -5,6 +6,18 @@ function nextSequence() {
   const pickedColor = buttonColors[rand];
   gamePattern.push(pickedColor);
   console.log(gamePattern);
+  $("#" + pickedColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  $("#test").on("click", function () {
+    let audio = new Audio("sounds/" + pickedColor + ".mp3");
+    audio.play();
+  });
 }
-
-nextSequence();
+// nextSequence();
+$(".btn").click(function (e) {
+  const userChosenColor = $(e.target).attr("id");
+  userClickedPattern.push(userChosenColor);
+  console.log(userClickedPattern);
+});
